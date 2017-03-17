895ba3e265e4b6027f269ea55e9373afa95ca7e3
xie-lab-win10 <s.z.xie@outlook.com>
2017-03-17 18:28:13 +0800

    初始化仓库，第一次提交。

---
+++ src/HelloJava.java
@@ -0,0 +1,9 @@
+/**
+ * Hello Java file.
+ * Created by Xie on 2017.3.17.
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
