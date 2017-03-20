ac11cb1f59a3466da3e9fb55e7e93b052ab8c228
xie-lab-win10 <s.z.xie@outlook.com>
2017-03-20 16:45:00 +0800

    学习完mooc视频中的集合类。

--- src/com/collection/AddingGroups.java
+++ src/com/collection/AddingGroups.java
@@ -1,3 +1,5 @@
+package com.collection;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -8,7 +10,7 @@ import java.util.Collections;
  */
 public class AddingGroups {
     public static void main(String[] args) {
-        Collection<Integer> collection = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        Collection<Integer> collection = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
         Integer[] moreInts = {6, 7, 8, 9, 10};
         collection.addAll(Arrays.asList(moreInts));
 
@@ -19,5 +21,7 @@ public class AddingGroups {
             System.out.print(i + " ");
 
         System.out.print(collection);
+
     }
+
 }
--- src/com/collection/ApplesAndOrangesWithoutGenerics.java
+++ src/com/collection/ApplesAndOrangesWithoutGenerics.java
@@ -1,3 +1,5 @@
+package com.collection;
+
 import java.util.ArrayList;
 
 /**
